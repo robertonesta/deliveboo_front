d8115535ccab162c425dce67f758593ae1e62bed
RobertoNesta <robertonesta1993@gmail.com>
2023-10-23 15:45:59 +0200

    vite structure initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
